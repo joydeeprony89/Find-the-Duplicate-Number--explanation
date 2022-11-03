672bfd8d4f7f249d47da3875e8a25fa8bf995a9b
jd_786 <jaydeep_shil@certree.com>
2022-11-03 20:28:19 +0530

    Add project files.

---
+++ Find the Duplicate Number/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Find_the_Duplicate_Number
+{
+  class Program
+  {
+    /*
+     * Take the array [1,3,4,2] as an example, the index of this array is [0, 1, 2, 3], we can map the index to the nums[n].
+     *0->1
+      1->3
+      2->4
+      3->2
+    * 0->1->3->2->4->null
+    * If there are a repeated numbers in the array, take the array [1,3,4,2,2] as an example,
+    * 0->1
+      1->3
+      2->4
+      3->2
+      4->2
+    *Similarly, a linked list is like this : 0->1->3->2->4->2->4->2->….......................
+    *Here 2->4 is a cycle,
+     */
+    static void Main(string[] args)
+    {
+      var nums = new int[5] { 4, 3, 1, 4, 2 };
+      Solution s = new Solution();
+      var answer = s.FindDuplicate(nums);
+      Console.WriteLine(answer);
+    }
+  }
+
+  public class Solution
+  {
+    public int FindDuplicate(int[] nums)
+    {
+      int slow = 0;
+      int fast = 0;
+      do
+      {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+      } while (slow != fast);
+
+      slow = 0;
+      while(slow != fast)
+      {
+        slow = nums[slow];
+        fast = nums[fast];
+      }
+
+      return slow;
+    }
+  }
+}
